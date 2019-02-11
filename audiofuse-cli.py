f0375dee3bff884af0a8602f64dbe4c81b229188
Richard Alex Hofer <rofer@rofer.me>
2019-02-10 22:49:32 -0500

    Add much better digital I/O switching support.
    This seems to work, but occasionally the digital I/O gets into a
    state the program can no longer recognize.

--- audiofuse-cli.py
+++ audiofuse-cli.py
@@ -1,18 +1,47 @@
 #!/usr/bin/python3
 import argparse
+from enum import Enum, unique
 import sys
 
 import usb.core
 from usb.core import USBError
-from usb.util import CTRL_OUT, CTRL_TYPE_CLASS, CTRL_RECIPIENT_INTERFACE
+from usb.util import CTRL_IN, CTRL_OUT, CTRL_TYPE_CLASS, CTRL_RECIPIENT_INTERFACE
 
 ARTURIA_VENDOR_ID = 0x1c75
 AUDIOFUSE_PRODUCT_ID = 0xaf02
 
+Input = Enum(value='Input', names=[
+    ('Unknown', 0),
+    ('SPDIF_coax', 1),
+    ('spdif-coax', 1),
+    ('SPDIF_optical', 2),
+    ('SPDIF-optical', 2),
+    ('ADAT', 3),
+    ('adat', 3),
+    ('WClock', 4),
+    ('wclock', 4),
+    ])
+Output = Enum(value='Output', names=[
+    ('Unknown', 0),
+    ('SPDIF', 1),
+    ('spdif', 1),
+    ('ADAT', 2),
+    ('adat', 2),
+    ('WClock', 3),
+    ('wclock', 3),
+    ])
+
+class RequiresAllowRestart(Exception):
+    "Thrown when a restart is attempted without the allow_restart flag."
+    pass
+
 class AudioFuse:
     """A class for controlling a single AudioFuse device."""
 
-    def __new__(cls, verbose):
+    input = Input.Unknown
+    output = Output.Unknown
+
+    def __new__(cls, verbose, allow_restart):
         dev = usb.core.find(idVendor=ARTURIA_VENDOR_ID, idProduct=AUDIOFUSE_PRODUCT_ID)
         if dev is None:
             return(None)
@@ -20,33 +49,89 @@ class AudioFuse:
         af._dev = dev
         af._reattach_interfaces = set()
         af._verbose = verbose
+        af._allow_restart = allow_restart
+        af.attach()
+        af.get_initial_status()
         return(af)
 
-    def set_digital_in(self, val):
-        # bRequest: 3
-        # wValue: 0x0005 
-        # Data: 
-        #  0: S/PDIF (coax/optical) (must be differentiated with something else)
-        #  1: ADAT
-        #  2: W. Clock
-        # TODO: The bits in-between
-        self.setup()
-
-    def set_digital_out(self, val):
-        if val == "spdif":
+    def _change_digital_in(self, val):
+        "Send the actual USB packets to the Audiofuse to change the digital in setting."
+        if val == Input.SPDIF_coax:
             data = 0
-        elif val == "adat":
+            data2 = 0
+        if val == Input.SPDIF_optical:
+            data = 0
+            data2 = 1
+        elif val == Input.ADAT:
             data = 1
-        elif val == "wclock":
+            data2 = 1
+            # 0x0105 = 2 at the end when restart needed
+        elif val == Input.WClock:
+            data = 2
+            data2 = 0
+        else:
+            raise ValueError('Unknown value (%s) is not "spdif-coax", "spdif-optical", "adat", or "wclock"' % val)
+
+        request_type = usb.util.build_request_type(CTRL_OUT, CTRL_TYPE_CLASS, CTRL_RECIPIENT_INTERFACE)
+        self._dev.ctrl_transfer(request_type, 0x03, 0x0005, 0x4600, [data])
+        self._dev.ctrl_transfer(request_type, 0x03, 0x0305, 0x4600, [data2])
+        self._dev.ctrl_transfer(request_type, 0x03, 0x0c05, 0x4600, [0])
+        self._dev.ctrl_transfer(request_type, 0x03, 0x0300, 0x4c00, [0, 0])
+        self._dev.ctrl_transfer(request_type, 0x03, 0x0200, 0x4c00, [0, 0])
+
+    def set_digital_in(self, val):
+        if val == self.input:
+            if self._verbose:
+                print("Input already set correctly. Skipping.")
+                return
+
+        restart = False
+        # Restart when we would be switching from no ADAT I/O to some ADAT I/O
+        # or from some ADAT I/O to no ADAT I/O.
+        if (val == Input.ADAT and self.input != Input.ADAT and self.output != Output.ADAT) or (
+                val != Input.ADAT and self.input == Input.ADAT and self.output != Output.ADAT):
+            restart = True
+            if not self._allow_restart:
+                raise RequiresAllowRestart
+
+        self._change_digital_in(val)
+        if restart:
+            self._change_digital_out(self.output)
+
+    def _change_digital_out(self, val):
+        "Send the actual USB packets to the Audiofuse to change the digital out setting."
+        if val == Output.SPDIF:
+            data = 0
+        elif val == Output.ADAT:
+            data = 1
+        elif val == Output.WClock:
             data = 2
         else:
             raise ValueError('Unknown value (%s) is not "spdif", "adat", or "wclock"' % val)
 
-        self.setup()
         request_type = usb.util.build_request_type(CTRL_OUT, CTRL_TYPE_CLASS, CTRL_RECIPIENT_INTERFACE)
         self._dev.ctrl_transfer(request_type, 0x03, 0x0105, 0x4600, [data])
 
-    def setup(self):
+    def set_digital_out(self, val):
+        if val == self.output:
+            if self._verbose:
+                print("Output already set correctly. Skipping.")
+                return
+
+        restart = False
+        # Restart when we would be switching from no ADAT I/O to some ADAT I/O
+        # or from some ADAT I/O to no ADAT I/O.
+        if (val == Output.ADAT and self.output != Output.ADAT and self.input != Input.ADAT) or (
+                val != Output.ADAT and self.output == Output.ADAT and self.input != Input.ADAT):
+            restart = True
+            if not self._allow_restart:
+                raise RequiresAllowRestart
+
+        self._change_digital_out(val)
+        if restart:
+            self._change_digital_in(self.input)
+
+    def attach(self):
         # Detach kernel driver if currently attached
         for i in range(self._dev[0].bNumInterfaces): # Check all interfaces
             if self._dev.is_kernel_driver_active(i):
@@ -58,24 +143,78 @@ class AudioFuse:
         # The AudioFuse only has one configuration
         self._dev.set_configuration()
 
+    def get_initial_status(self):
+        class TemplateMatchError(Exception):
+            "Thrown when we're unable to match any of the given templates."
+            pass
+        def match_template(arr, templates):
+            checked_bytes = {}
+            for (tmpl_val, tmpl_bytes) in templates:
+                bad_match = False
+                for (byte_num, val) in tmpl_bytes:
+                    b = arr[byte_num]
+                    checked_bytes[byte_num] = b
+                    if b != val:
+                        bad_match = True
+                        break
+                if bad_match:
+                    continue
+                return(tmpl_val)
+            raise TemplateMatchError(checked_bytes)
+
+        input_templates = [
+            (Input.SPDIF_coax,    [(23, 0), (29, 0)]),
+            (Input.SPDIF_optical, [(23, 1), (29, 0)]),
+            (Input.ADAT,          [(29, 1)]),
+            (Input.WClock,        [(29, 2)]),
+        ]
+        output_templates = [
+            (Output.SPDIF,  [(28, 0), (30, 0)]),
+            (Output.ADAT,   [(28, 0), (30, 1)]),
+            (Output.WClock, [(28, 1), (30, 2)]),
+        ]
+
+        # Request the status of the Audiofuse
+        request_type = usb.util.build_request_type(CTRL_IN, CTRL_TYPE_CLASS, CTRL_RECIPIENT_INTERFACE)
+        status_bytes = bytearray(self._dev.ctrl_transfer(request_type, 0x03, 0, 0x4700, 178))
+        if self._verbose:
+            print("Initial Status data:", status_bytes.hex())
+
+        # Interpret the status bytes and set class members accordingly
+        try:
+            self.input = match_template(status_bytes, input_templates)
+        except TemplateMatchError as e:
+            print("Could not identify input: %s" % e)
+        try:
+            self.output = match_template(status_bytes, output_templates)
+        except TemplateMatchError as e:
+            print("Could not identify output: %s" % e)
+        print("AudioFuse Digital I/O set to %s and %s" % (self.input, self.output))
+
     def __del__(self):
         usb.util.dispose_resources(self._dev)
 
         # Reattach kernel driver if we detached it
-        # TODO: Figure out why reattaching sometimes fails with Resource Busy
         for i in self._reattach_interfaces:
             if self._verbose:
                 print("Reattaching kernel driver to interface %d." % i)
-            self._dev.attach_kernel_driver(i)
+            try:
+                self._dev.attach_kernel_driver(i)
+            except USBError as e:
+                if e.errno is 16:
+                    print("TODO: Figure out why we get resource busy errors here.")
+                else:
+                    raise
 
 def main():
     parser = argparse.ArgumentParser(description="An unofficial, incomplete CLI for controlling the Arturia AudioFuse.")
     parser.add_argument("--digital_in", "--din", choices=["spdif-coax", "spdif-optical", "adat", "wclock"])
     parser.add_argument("--digital_out", "--dout", choices=["spdif", "adat", "wclock"])
     parser.add_argument("-v", action='store_true')
+    parser.add_argument("--allow_restart", "-r", action='store_true')
     args = parser.parse_args()
 
-    af = AudioFuse(args.v)
+    af = AudioFuse(args.v, args.allow_restart)
     if af:
         print("Found an AudioFuse.")
     else:
@@ -84,9 +223,17 @@ def main():
     
     try:
         if args.digital_out:
-            af.set_digital_out(args.digital_out)
+            try:
+                af.set_digital_out(Output[args.digital_out])
+            except RequiresAllowRestart:
+                print("Setting digital out to %s requires a restart. Please re-run with --allow_restart." %
+                        args.digital_out)
         if args.digital_in:
-            af.set_digital_in(args.digital_in)
+            try:
+                af.set_digital_in(Input[args.digital_in])
+            except RequiresAllowRestart:
+                print("Setting digital in to %s requires a restart. Please re-run with --allow_restart." %
+                        args.digital_in)
     except USBError as e:
         if e.errno is 13:
             print("Insufficient permission to talk to AudioFuse.")
