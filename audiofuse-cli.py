b6b8ff2ee7592951a0936298488c616555baa6bb
Richard Alex Hofer <rofer@rofer.me>
2019-02-16 18:56:57 -0500

    Only restart when necessary.
    Previously by detaching and re-attaching all kernel drivers we were
    triggering restarts every run. This fixes that and now correctly
    triggers restarts only when necessary.

--- audiofuse-cli.py
+++ audiofuse-cli.py
@@ -51,10 +51,29 @@ class AudioFuse:
         af._verbose = verbose
         af._allow_restart = allow_restart
         af.attach()
-        af.get_initial_status()
+        af.get_status()
+        af._send_300_200()
         return(af)
 
-    def _change_digital_in(self, val):
+    def _restart(self, to_adat):
+        request_type = usb.util.build_request_type(CTRL_OUT, CTRL_TYPE_CLASS, CTRL_RECIPIENT_INTERFACE)
+        if to_adat:
+            # No idea why we only need this here
+            self._dev.ctrl_transfer(request_type, 0x01, 0x0100, 0x2900, [0, 0x77, 1, 0])
+            data = 2
+        else:
+            data = 1
+        self._dev.ctrl_transfer(request_type, 0x03, 0, 0x5000, [data, 0])
+        # Don't try and reattach interfaces after a restart
+        self._reattach_interfaces = set()
+
+    def _send_300_200(self):
+        # This is done in several places, but I have no diea why.
+        request_type = usb.util.build_request_type(CTRL_OUT, CTRL_TYPE_CLASS, CTRL_RECIPIENT_INTERFACE)
+        self._dev.ctrl_transfer(request_type, 0x03, 0x0300, 0x4c00, [0, 0])
+        self._dev.ctrl_transfer(request_type, 0x03, 0x0200, 0x4c00, [0, 0])
+
+    def _change_digital_in(self, val, restart, to_adat):
         "Send the actual USB packets to the Audiofuse to change the digital in setting."
         if val == Input.SPDIF_coax:
             data = 0
@@ -65,7 +84,6 @@ class AudioFuse:
         elif val == Input.ADAT:
             data = 1
             data2 = 1
-            # 0x0105 = 2 at the end when restart needed
         elif val == Input.WClock:
             data = 2
             data2 = 0
@@ -75,9 +93,16 @@ class AudioFuse:
         request_type = usb.util.build_request_type(CTRL_OUT, CTRL_TYPE_CLASS, CTRL_RECIPIENT_INTERFACE)
         self._dev.ctrl_transfer(request_type, 0x03, 0x0005, 0x4600, [data])
         self._dev.ctrl_transfer(request_type, 0x03, 0x0305, 0x4600, [data2])
+        # My reverse-engineering says we should call restart here, but then we
+        # don't have time to finish these transfers first.
+        #if restart:
+        #    self._restart(to_adat)
         self._dev.ctrl_transfer(request_type, 0x03, 0x0c05, 0x4600, [0])
-        self._dev.ctrl_transfer(request_type, 0x03, 0x0300, 0x4c00, [0, 0])
-        self._dev.ctrl_transfer(request_type, 0x03, 0x0200, 0x4c00, [0, 0])
+        self._send_300_200()
+        if restart:
+            self._change_digital_out(self.output, False, False)
+            # Restarting here instead seems to work.
+            self._restart(to_adat)
 
     def set_digital_in(self, val):
         if self._verbose:
@@ -90,17 +115,18 @@ class AudioFuse:
         restart = False
         # Restart when we would be switching from no ADAT I/O to some ADAT I/O
         # or from some ADAT I/O to no ADAT I/O.
-        if (val == Input.ADAT and self.input != Input.ADAT and self.output != Output.ADAT) or (
-                val != Input.ADAT and self.input == Input.ADAT and self.output != Output.ADAT):
+        if val == Input.ADAT and self.input != Input.ADAT and self.output != Output.ADAT:
             restart = True
-            if not self._allow_restart:
-                raise RequiresAllowRestart
+            to_adat = True
+        if val != Input.ADAT and self.input == Input.ADAT and self.output != Output.ADAT:
+            restart = True
+            to_adat = False
+        if restart and not self._allow_restart:
+            raise RequiresAllowRestart
 
-        self._change_digital_in(val)
-        if restart:
-            self._change_digital_out(self.output)
+        self._change_digital_in(val, restart, to_adat)
 
-    def _change_digital_out(self, val):
+    def _change_digital_out(self, val, restart, to_adat):
         "Send the actual USB packets to the Audiofuse to change the digital out setting."
         if val == Output.SPDIF:
             data = 0
@@ -113,6 +139,13 @@ class AudioFuse:
 
         request_type = usb.util.build_request_type(CTRL_OUT, CTRL_TYPE_CLASS, CTRL_RECIPIENT_INTERFACE)
         self._dev.ctrl_transfer(request_type, 0x03, 0x0105, 0x4600, [data])
+        if restart:
+            # My reverse-engineering says we should call restart here, but then we
+            # don't have time to finish these transfers first.
+            # self._restart(to_adat)
+            self._change_digital_in(self.input, False, False)
+            # Doing it afterwards seems to still work.
+            self._restart(to_adat)
 
     def set_digital_out(self, val):
         if self._verbose:
@@ -125,17 +158,16 @@ class AudioFuse:
         restart = False
         # Restart when we would be switching from no ADAT I/O to some ADAT I/O
         # or from some ADAT I/O to no ADAT I/O.
-        if (val == Output.ADAT and self.output != Output.ADAT and self.input != Input.ADAT) or (
-                val != Output.ADAT and self.output == Output.ADAT and self.input != Input.ADAT):
+        if val == Output.ADAT and self.output != Output.ADAT and self.input != Input.ADAT:
             restart = True
-            if not self._allow_restart:
-                raise RequiresAllowRestart
+            to_adat = True
+        if val != Output.ADAT and self.output == Output.ADAT and self.input != Input.ADAT:
+            restart = True
+            to_adat = False
+        if restart and not self._allow_restart:
+            raise RequiresAllowRestart
 
-        self._change_digital_out(val)
-        if restart:
-            self._change_digital_in(self.input)
-            # TODO: Wait and reconnect after restart. Otherwise a subsequent
-            # set_digital_in will fail.
+        self._change_digital_out(val, restart, to_adat)
 
     def _set_binary_option(self, name, usb_val, val):
         if self._verbose:
@@ -157,18 +189,13 @@ class AudioFuse:
         self._set_binary_option("ground lift", 0x0c00, val)
 
     def attach(self):
-        # Detach kernel driver if currently attached
-        for i in range(self._dev[0].bNumInterfaces): # Check all interfaces
-            if self._dev.is_kernel_driver_active(i):
-                if self._verbose:
-                    print("Detaching from interface %d" % i)
-                self._reattach_interfaces |= {i}
-                self._dev.detach_kernel_driver(i)
+        if self._dev.is_kernel_driver_active(0):
+            if self._verbose:
+                print("Detaching from interface %d" % 0)
+            self._reattach_interfaces |= {0}
+            self._dev.detach_kernel_driver(0)
 
-        # The AudioFuse only has one configuration
-        self._dev.set_configuration()
-
-    def get_initial_status(self):
+    def get_status(self):
         class TemplateMatchError(Exception):
             "Thrown when we're unable to match any of the given templates."
             pass
@@ -203,7 +230,7 @@ class AudioFuse:
         request_type = usb.util.build_request_type(CTRL_IN, CTRL_TYPE_CLASS, CTRL_RECIPIENT_INTERFACE)
         status_bytes = bytearray(self._dev.ctrl_transfer(request_type, 0x03, 0, 0x4700, 178))
         if self._verbose:
-            print("Initial Status data:", status_bytes.hex())
+            print("Status data:", status_bytes.hex())
 
         # Interpret the status bytes and set class members accordingly
         try:
@@ -226,8 +253,10 @@ class AudioFuse:
             try:
                 self._dev.attach_kernel_driver(i)
             except USBError as e:
-                if e.errno is 16:
-                    print("TODO: Figure out why we get resource busy errors here.")
+                if e.errno is 2:
+                    # This seems to happen whenever this is run after a
+                    # self-initiated restart.
+                    print("TODO: Figure out why we get entity not found here.")
                 else:
                     raise
 
@@ -247,61 +276,65 @@ def main():
 
     try:
         af = AudioFuse(args.verbose, args.allow_restart)
-        if af:
-            print("Found an AudioFuse.")
-        else:
-            print("No AudioFuse found.")
-            sys.exit(1)
-    
-        # Validate arguments
-        if args.from_phone_2 and args.not_from_phone_2:
-            print("Cannot simultaneously set --from-phone-2 and --not-from-phone-2.")
-            sys.exit(1)
-
-        if args.reamping and args.no_reamping:
-            print("Cannot simultaneously set --reamping and --no-reamping.")
-            sys.exit(1)
-
-        if args.ground_lift and args.no_ground_lift:
-            print("Cannot simultaneously set --ground-lift and --no-ground-lift.")
-            sys.exit(1)
-
-        if args.digital_out:
-            try:
-                af.set_digital_out(Output[args.digital_out])
-            except RequiresAllowRestart:
-                print("Setting digital out to %s requires a restart. Please re-run with --allow_restart." %
-                        args.digital_out)
-        if args.digital_in:
-            try:
-                af.set_digital_in(Input[args.digital_in])
-            except RequiresAllowRestart:
-                print("Setting digital in to %s requires a restart. Please re-run with --allow_restart." %
-                        args.digital_in)
-
-        if args.from_phone_2:
-            af.set_from_phone_2(1)
-
-        if args.not_from_phone_2:
-            af.set_from_phone_2(0)
-
-        if args.reamping:
-            af.set_reamping(1)
-
-        if args.no_reamping:
-            af.set_reamping(0)
-
-        if args.ground_lift:
-            af.set_ground_lift(1)
-
-        if args.no_ground_lift:
-            af.set_ground_lift(0)
-
     except USBError as e:
         if e.errno is 13:
             print("Insufficient permission to talk to AudioFuse.")
             sys.exit(1)
+        elif e.errno is 16:
+            print("AudioFuse is currently in use by another application.")
+            sys.exit(1)
         raise
+
+    if af:
+        print("Found an AudioFuse.")
+    else:
+        print("No AudioFuse found.")
+        sys.exit(1)
+
+    # Validate arguments
+    if args.from_phone_2 and args.not_from_phone_2:
+        print("Cannot simultaneously set --from-phone-2 and --not-from-phone-2.")
+        sys.exit(1)
+
+    if args.reamping and args.no_reamping:
+        print("Cannot simultaneously set --reamping and --no-reamping.")
+        sys.exit(1)
+
+    if args.ground_lift and args.no_ground_lift:
+        print("Cannot simultaneously set --ground-lift and --no-ground-lift.")
+        sys.exit(1)
+
+    if args.from_phone_2:
+        af.set_from_phone_2(1)
+
+    if args.not_from_phone_2:
+        af.set_from_phone_2(0)
+
+    if args.reamping:
+        af.set_reamping(1)
+
+    if args.no_reamping:
+        af.set_reamping(0)
+
+    if args.ground_lift:
+        af.set_ground_lift(1)
+
+    if args.no_ground_lift:
+        af.set_ground_lift(0)
+
+    # Do these last since they can potentially lead to a restart.
+    if args.digital_out:
+        try:
+            af.set_digital_out(Output[args.digital_out])
+        except RequiresAllowRestart:
+            print("Setting digital out to %s requires a restart. Please re-run with --allow_restart." %
+                    args.digital_out)
+    if args.digital_in:
+        try:
+            af.set_digital_in(Input[args.digital_in])
+        except RequiresAllowRestart:
+            print("Setting digital in to %s requires a restart. Please re-run with --allow_restart." %
+                    args.digital_in)
 
 
 if __name__ == "__main__":
